b67efa144a748f78dd171ef3a0d87350cf6e9612
DevM7mdAli <74423891+DevM7mdAli@users.noreply.github.com>
2025-04-16 18:34:48 +0300

    adding the supabase

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,23 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
+import 'react-native-url-polyfill/auto'
+import { useState, useEffect } from 'react'
+import { supabase } from '../lib/supabase'
+import Auth from '../components/Auth'
+import { View, Text } from 'react-native'
+import { Session } from '@supabase/supabase-js'
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
   return (
-    <View className="flex-1 bg-red-400 justify-center items-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Auth />
+      {session && session.user && <Text>{session.user.role}</Text>}
     </View>
-  );
+  )
 }
--- components/ui/text.tsx
+++ components/ui/text.tsx
@@ -1,4 +1,4 @@
-import cn from "@/libs/utils";
+import cn from "@/lib/utils";
 import { Text, TextProps } from "react-native";
 
 interface TextFontProps extends TextProps {
