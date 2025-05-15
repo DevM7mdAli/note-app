a2ed261e7026c2dfc85a2306c0b0271ba732a9d3
DevM7mdAli <74423891+DevM7mdAli@users.noreply.github.com>
2025-05-15 17:31:47 +0300

    adding the tabBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,41 @@
 import { Tabs } from "expo-router";
+import { MaterialIcons } from '@expo/vector-icons'
 
 
 const Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+    screenOptions={{
+      tabBarActiveTintColor: "#d81313",
+      tabBarInactiveTintColor: "#5E5E5E",
+      tabBarHideOnKeyboard: true,
+    }}
+    >
       <Tabs.Screen 
       name="index"
       options={{
-        headerShown: false
+        headerShown: false,
+        tabBarIcon: ({ focused, color }) => ( 
+        <MaterialIcons 
+        name="notes" 
+        size={24} 
+        color={color} 
+        /> 
+      ),
+        title: 'Notes'
       }}
       />
       <Tabs.Screen 
       name="settings"
       options={{
-        headerShown: false
+        headerShown: false,
+        tabBarIcon: ({ focused, color }) => ( 
+        <MaterialIcons 
+        name="settings" 
+        size={24} 
+        color={color}/> 
+      ),
+        title: 'Setting'
       }}
       />
     </Tabs>
